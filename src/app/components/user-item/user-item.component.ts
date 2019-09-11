dd91bbb72edefbe67cc0bac11fbbd9cb047c16c6
OmoGr3y <anthonyomoijahe@gmail.com>
2019-09-11 12:04:04 +0100

    added return false so the browser doesn't refresh the page

--- src/app/components/user-item/user-item.component.ts
+++ src/app/components/user-item/user-item.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
   template: `
-    <form class="ui large form segment">
-        <h3 class="ui header">Add a link</h3>
-        
-        <div class="field">
-            <label for="title">Title:</label>
-            <input name="title" id="title">
+      <form class="ui large form segment">
+          <h3 class="ui header">Add a link</h3>
+          <div class="field">
+              <label for="title">Title:</label>
+              <input name="title" id="title" #newtitle>
+          </div>
+          <div class="field">
+              <label for="link">Link:</label>
+              <input name="link" id="link" #newlink>
+          </div>
+          <button (click)="addArticle(newtitle, newlink)" class="ui positive right floated button">
+              Submit Link
+          </button>
+      </form>
+        <div class="ui grid posts">
+            <app-article></app-article>
         </div>
-        <div class="field">
-            <label for="link">Link:</label>
-            <input name="link" id="link">
-        </div>
-    </form>
   `,
   styles: []
 })
 export class UserItemComponent implements OnInit {
   name: string;
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
 
   constructor() {
     this.name = 'Felipe';
--- src/app/ng-book2/ng-book2.module.ts
+++ src/app/ng-book2/ng-book2.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HelloWorldComponent } from '../components/hello-world/hello-world.component';
 import { UserItemComponent } from '../components/user-item/user-item.component';
+import { ArticleComponent } from '../components/article/article.component';
 
 
 
 @NgModule({
-  declarations: [HelloWorldComponent, UserItemComponent],
+  declarations: [HelloWorldComponent, UserItemComponent, ArticleComponent],
   exports: [
     HelloWorldComponent,
     UserItemComponent
