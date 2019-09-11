a2159383690d4a94c92d4ad35f4604bead4c1d25
OmoGr3y <anthonyomoijahe@gmail.com>
2019-09-11 12:39:00 +0100

    added functions to class article (law of demeter)

--- src/app/models/article.ts
+++ src/app/models/article.ts
@@ -8,4 +8,24 @@ export class Article {
     this.link = link;
     this.votes = votes || 0;
   }
+
+  voteUp(): void {
+    this.votes += 1;
+  }
+
+  voteDown(): void {
+    this.votes -= 1;
+  }
+
+  domain(): string {
+    try {
+      // e.g. http://foo.com/path/to/bar
+      const domainAndpath: string = this.link.split('//')[1];
+      // e.g foo.com/path/to/bar
+      return  domainAndpath.split('/')[0];
+    } catch (e) {
+      return  null;
+
+    }
+  }
 }
